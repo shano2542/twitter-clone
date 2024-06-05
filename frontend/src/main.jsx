2c38c5149d2f9d776dbf4bd54bdb73f0f9e2a5dc
Zeshan Ahmad <108078242+shano2542@users.noreply.github.com>
2024-06-05 19:21:09 +0500

    3= Auth handled and routes portected in client section

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+
+const queryClinet = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    }
+  }
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClinet}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
