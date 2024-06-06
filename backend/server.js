8d63671d00b124c15d1752499457237d34d8773b
Zeshan Ahmad <108078242+shano2542@users.noreply.github.com>
2024-06-07 00:57:25 +0500

    4- prepared for deployement

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -28,6 +29,8 @@ app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 
 // limit shouldn't be high to prevent DoS attack
 app.use(express.json({limit: "5mb"})); // to parse req.body for my own ease like protocol between client and server
@@ -39,6 +42,13 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 
 
