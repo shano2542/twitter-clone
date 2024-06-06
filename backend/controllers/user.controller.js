a0b138f999538c2d1f2a040dd042603e3c6124b5
Zeshan Ahmad <108078242+shano2542@users.noreply.github.com>
2024-06-06 17:01:49 +0500

    3- follow Unfollow hook added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -46,7 +46,7 @@ export const followUnfollowUser = async (req, res) => {
       await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
       await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
 
-      // TODO return the id of the user as a response
+      
 
       res.status(200).json({ message: "User unfollowed successfully" });
     } else {
@@ -65,7 +65,8 @@ export const followUnfollowUser = async (req, res) => {
 
       await newNotification.save();
 
-      // TODO return the id of the user as a response
+      
+      
       res.status(200).json({ message: "User Followed Successfully " });
     }
   } catch (error) {
