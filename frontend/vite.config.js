bbcf46c4ac70ca9664f47cbf041e22d05c19955b
Zeshan Ahmad <108078242+shano2542@users.noreply.github.com>
2024-06-07 01:22:38 +0500

    4- fixed build for vercel deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure this is set to 'dist'
+  },
   server: {
     port: 3000,
     proxy: {
